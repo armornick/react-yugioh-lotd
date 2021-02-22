ab56f5ea8df21b9ebe601830e020cccd63ac84d4
armornick <nick.arnoeyts@mail.com>
2021-02-22 07:26:16 +0100

    switch to next.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
-
-// ReactDOM.render(<PackList />, document.getElementById('app'));
-// ReactDOM.render(<CardList packName="Mai Valentine" />, document.getElementById('app'));
-ReactDOM.render(<App />, document.getElementById('app'));
-
