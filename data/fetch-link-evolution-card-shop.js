710bfaa767dd16f9a0eb3fcf087af020491736b0
armornick <nick.arnoeyts@mail.com>
2021-02-23 13:38:04 +0100

    equalize functionality of link evolution with lotd

--- data/fetch-link-evolution-card-shop.js
+++ data/fetch-link-evolution-card-shop.js
@@ -9,6 +9,25 @@ const OUTPUT_FNAME = 'link-evolution-boosters.json';
 
 const RE_CARD_TITLE = /^"(.+)"$/;
 
+const CARD_TYPES = {
+    // main deck monsters
+    "Effect Monster": "Effect Monsters", "Flip Effect Monster": "Effect Monsters",
+    "Flip Tuner Effect Monster":"Tuner monsters","Normal Tuner Monster":"Tuner monsters",
+    "Tuner Monster":"Tuner monsters","Gemini Monster":"Gemini monsters",
+    "Normal Monster":"Normal Monsters","Pendulum Effect Monster":"Pendulum Monsters",
+    "Pendulum Flip Effect Monster":"Pendulum Monsters","Pendulum Normal Monster":"Pendulum Monsters",
+    "Pendulum Tuner Effect Monster":"Pendulum Monsters","Ritual Effect Monster":"Ritual Monsters",
+    "Ritual Monster":"Ritual Monsters","Spirit Monster":"Spirit monsters",
+    "Toon Monster":"Toon monsters","Union Effect Monster":"Union monsters",
+    // extra deck monsters
+    "Fusion Monster":"Fusion Monsters","Pendulum Effect Fusion Monster":"Fusion Monsters",
+    "Link Monster":"Link Monsters","Synchro Monster":"Synchro Monsters",
+    "Synchro Pendulum Effect Monster":"Synchro Monsters","Synchro Tuner Monster":"Synchro Monsters",
+    "XYZ Monster":"Xyz Monsters","XYZ Pendulum Effect Monster":"Xyz Monsters",
+    // spells and trap cards
+    "Spell Card":"Spells","Trap Card":"Traps",
+};
+
 const mkdirIfNotExists = (directory) => {
     if (!existsSync(directory)) {
         mkdirSync(directory);
@@ -40,6 +59,8 @@ const main = async () => {
     mkdirIfNotExists(CACHE_DIR);
     mkdirIfNotExists(OUTPUT_DIR);
 
+    const cardDb = require('./yugioh-cards.json').data;
+
     const result = {};
     const dl = new Downloader({ outputDir: CACHE_DIR });
 
@@ -56,7 +77,7 @@ const main = async () => {
         const title = document.getElementById('firstHeading').textContent.trim().replace(/\s\(Link\sEvolution\)$/, '');
         console.log(title);
 
-        let temp = [];
+        const data = {}, archetypes = {};
         const tables = document.querySelectorAll('table.wikitable');
         for (const table of tables) {
             const heading = table.querySelector('th');
@@ -67,8 +88,25 @@ const main = async () => {
                         const firstChild = row.children[0];
                         if (firstChild && firstChild.tagName !== 'TH') {
 
-                            const cardTitle = RE_CARD_TITLE.exec( firstChild.textContent.trim() )[1];
-                            temp.push(cardTitle);
+                            let cardTitle = RE_CARD_TITLE.exec( firstChild.textContent.trim() )[1];
+                            let card = cardDb.find(card => card.name == cardTitle);
+                            if (!card) {
+                                card = cardDb.find(card => card.name.toUpperCase() == cardTitle.toUpperCase() );
+                                if (card) {
+                                    cardTitle = card.name;
+                                } else {
+                                    console.log(`could not find card "${cardTitle}"`);
+                                    continue;
+                                }
+                            }
+                            const cardType = CARD_TYPES[ card.type ];
+                            if (card.archetype) {
+                                archetypes[card.archetype] = archetypes[card.archetype] || 0;
+                                archetypes[card.archetype]++;
+                            }
+
+                            data[cardType] = data[cardType] || [];
+                            data[cardType].push(cardTitle);
 
                         }
                     }
@@ -76,7 +114,19 @@ const main = async () => {
             }
         }
 
-        result[ title ] = temp;
+        // find archetypes that booster pack is dedicated to
+        const archetypeList = [];
+        const foundArchetypes = Object.keys(archetypes);
+        for (let foundArchetype of foundArchetypes) {
+            if (archetypes[foundArchetype] > 5) { // arbitrary minimum
+                archetypeList.push(foundArchetype);
+            }
+        }
+        if (archetypeList.length > 1) {
+            data.archetypes = archetypeList;
+        }
+
+        result[ title ] = data;
     }
 
     console.log(`writing ${OUTPUT_FNAME}`);
