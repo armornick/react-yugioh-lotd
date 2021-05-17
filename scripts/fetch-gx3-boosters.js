830c4b031f133c231f75de1246b248ca755f1f57
armornick <nick.arnoeyts@mail.com>
2021-05-17 10:31:59 +0200

    add script to fetch spirit caller booster packs

---
+++ scripts/fetch-gx3-boosters.js
@@ -0,0 +1,62 @@
+const { writeFileSync, existsSync, mkdirSync } = require('fs');
+const { parseNavboxPage, parseRarityPage } = require('./yugipedia-parser');
+const Downloader = require('./axios-downloader');
+
+const CACHE_DIR = '.cache';
+const OUTPUT_DIR = 'data';
+const OUTPUT_FNAME = 'gx3-boosters.json';
+
+const mkdirIfNotExists = (directory) => {
+    if (!existsSync(directory)) {
+        mkdirSync(directory);
+    }
+};
+
+const RE_GX3_LINK = /_\(GX03-BP\)$/;
+
+const linkMatcher = (link) => {
+    return RE_GX3_LINK.test(link);
+}
+
+const parseLinks = (links) => {
+    const results = [];
+    for (const link of links) {
+        const name = link.replace(RE_GX3_LINK,'').replace(/^\/wiki\//, '');
+        results.push({
+            name: name.replace(/_/g, ' ').replace('%27', '\''),
+            fname: 'GX3_' + name + '.html',
+            url: 'https://yugipedia.com' + link,
+        });
+    }
+    return results;
+};
+
+const main = async () => {
+
+    mkdirIfNotExists(CACHE_DIR);
+    mkdirIfNotExists(OUTPUT_DIR);
+
+    const dl = new Downloader({ outputDir: CACHE_DIR });
+    const response = await dl.download('GX3-Boosters.html', 'https://yugipedia.com/wiki/Template:Yu-Gi-Oh!_GX_Spirit_Caller_sets');
+    
+    const linksRaw = parseNavboxPage(response, linkMatcher);
+    const links = parseLinks(linksRaw);
+
+    const result = {};
+    for (const link of links) {
+
+        const page = await dl.download(link.fname, link.url);
+        const data = parseRarityPage(page);
+
+        if (data.Common) {
+            result[link.name] = data;
+        }
+
+    }
+
+    console.log(`writing ${OUTPUT_FNAME}`);
+    writeFileSync(`${OUTPUT_DIR}/${OUTPUT_FNAME}`, JSON.stringify(result, null, 2));
+
+};
+
+main();
